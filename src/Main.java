1e90a8729b15cb09c4a4a0a631c7e75ccd6c80fb
Haley Bowers <Haley Bowers>
2016-01-18 19:19:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Haley on 1/12/2016.
+ */
+
+class Hello {
+    public static void main(String[] arguments){
+        //beginning
+        System.out.println("1302 Lab 1");
+        System.out.println("Haley Bowers");
+        String moo = "First day of class";
+        System.out.println(moo);
+        moo = "Jan. 12th 2016";
+        System.out.println(moo);
+    }
+}
